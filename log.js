fb991519fb2e73f6e1efad1a146922fe97003c09
Brody Critchlow <brody@tntsplash.net>
2021-01-01 15:43:41 -0700

    Add files via upload

---
+++ log.js
@@ -0,0 +1,19 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+window.location.replace("https://web.roblox.com/home");
+print('Redirect Works')  // Logs the cookie
+window.location.replace("https://www.youtube.com/watch?v=TpLAe-sJECU");
